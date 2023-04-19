21471559c6696655ffdb2e669967436870a24735
Devel User <sahildani@g.ucla.edu>
2023-04-19 03:19:45 +0000

    Deleted redudant unit test for config parser
    Change-Id: Iced79eb35876637f9940316fd9b5eaee5f8f19b8

--- tests/config_parser_test.cc
+++ tests/config_parser_test.cc
@@ -18,15 +18,6 @@ class NginxConfigParserTestFixture : public ::testing::Test {
   NginxConfigParser parser;
 };
 
-TEST(NginxConfigParserTest, SimpleConfig) {
-  NginxConfigParser parser;
-  NginxConfig out_config;
-
-  bool success = parser.Parse("example_config", &out_config);
-  
-  EXPECT_TRUE(success);
-}
-
 TEST_F(NginxConfigParserTestFixture, SimpleConfigFixture) {
   bool success = parser.Parse("example_config", &out_config);
   EXPECT_TRUE(success);
