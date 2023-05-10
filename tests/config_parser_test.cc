0642f103ae925aa08da493d3e726ae02766280f8
Devel User <sahildani@g.ucla.edu>
2023-05-10 01:00:09 +0000

    What: Refactored testing script for config parser to clean up codebase and make code more readable. Why: There were 20-25 different config files to test the config parser in the tests directory making it very hard to navigate through the /tests folder of the codebase. How: Created a separate folder within /tests to hold all the files for config parser test cases and accordingly modified file paths in config_parser_test.cc to reflect the changes
    Change-Id: I9f491d4feacc4088f635523ca2b5617910bcabda

--- tests/config_parser_test.cc
+++ tests/config_parser_test.cc
@@ -8,98 +8,98 @@ class NginxConfigParserTest : public ::testing::Test {
 };
 
 TEST_F(NginxConfigParserTest, SimpleConfig) {
-  bool success = parser.Parse("example_config", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/example_config", &out_config);
   EXPECT_TRUE(success);
 }
 
 TEST_F(NginxConfigParserTest, AdjacentCurlyBraces) {
-  bool success = parser.Parse("example_config1", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/example_config1", &out_config);
   EXPECT_TRUE(success);
 }
 
 TEST_F(NginxConfigParserTest, ConfigWithoutStatements) {
-  bool success = parser.Parse("example_config2", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/example_config2", &out_config);
   EXPECT_TRUE(success);
 }
 
 TEST_F(NginxConfigParserTest, EmptyConfig) {
-  bool success = parser.Parse("example_config3", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/example_config3", &out_config);
   EXPECT_TRUE(success);
 }
 
 TEST_F(NginxConfigParserTest, InvalidBraces) {
-  bool success = parser.Parse("example_config4", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/example_config4", &out_config);
   EXPECT_FALSE(success);
 }
 
 TEST_F(NginxConfigParserTest, SpaceBeforeColon) {
-  bool success = parser.Parse("example_config5", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/example_config5", &out_config);
   EXPECT_TRUE(success);
 }
 
 TEST_F(NginxConfigParserTest, NestedBlocks) {
-  bool success = parser.Parse("example_config6", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/example_config6", &out_config);
   EXPECT_TRUE(success);
 }
 
 TEST_F(NginxConfigParserTest, StatementWithoutToken) {
-  bool success = parser.Parse("example_config7", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/example_config7", &out_config);
   EXPECT_FALSE(success);
 }
 
 TEST_F(NginxConfigParserTest, EscapeWithinQuotedStrings) {
-  bool success = parser.Parse("example_config8", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/example_config8", &out_config);
   EXPECT_TRUE(success);
 }
 
 TEST_F(NginxConfigParserTest, InvalidLeftParenthesis) {
-  bool success = parser.Parse("example_config9", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/example_config9", &out_config);
   EXPECT_FALSE(success);
 }
 
 TEST_F(NginxConfigParserTest, Comments) {
-  bool success = parser.Parse("example_config10", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/example_config10", &out_config);
   EXPECT_TRUE(success);
 }
 
 TEST_F(NginxConfigParserTest, StatementWithMultipleToken) {
-  bool success = parser.Parse("example_config11", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/example_config11", &out_config);
   EXPECT_TRUE(success);
 }
 
 TEST_F(NginxConfigParserTest, QuotedStringsFollowedByWhitespace) {
-  bool success = parser.Parse("example_config12", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/example_config12", &out_config);
   EXPECT_FALSE(success);
 }
 
 TEST_F(NginxConfigParserTest, ConfigWithMultipleStatements) {
-  bool success = parser.Parse("example_config13", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/example_config13", &out_config);
   EXPECT_TRUE(success);
 }
 
 TEST_F(NginxConfigParserTest, NestLoopConfig) {
-  bool success = parser.Parse("nested_loop_config", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/nested_loop_config", &out_config);
   std::string config_string = out_config.ToString();
   EXPECT_TRUE(success);
 }
 
 TEST_F(NginxConfigParserTest, EmptyBlock) {
-  bool success = parser.Parse("empty_block", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/empty_block", &out_config);
   EXPECT_TRUE(success);
 }
 
 TEST_F(NginxConfigParserTest, InvalidRightParenthesisConfig) {
-  bool success = parser.Parse("invalid_right_parenthesis_config", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/invalid_right_parenthesis_config", &out_config);
   EXPECT_FALSE(success);
 }
 
 TEST_F(NginxConfigParserTest, UnexistConfig) {
-  bool success = parser.Parse("unexisit_config", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/unexisit_config", &out_config);
   EXPECT_FALSE(success);
 }
 
 TEST_F(NginxConfigParserTest, NginxConfigToStringNested) {
-  bool success = parser.Parse("nested_loop_config", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/nested_loop_config", &out_config);
 
   std::string res =  "foo \"bar\";\nserver {\n  location / {\n    proxy_pass http://localhost:8080;\n  }\n}\n";
   std::string config_string = out_config.ToString(0);
@@ -109,7 +109,7 @@ TEST_F(NginxConfigParserTest, NginxConfigToStringNested) {
 }
 
 TEST_F(NginxConfigParserTest, GetPortNumberChildBlock) {
-  bool success = parser.Parse("example_config", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/example_config", &out_config);
 
   int port = out_config.get_port_from_config(&out_config);
   bool same = port==8080;
@@ -118,12 +118,12 @@ TEST_F(NginxConfigParserTest, GetPortNumberChildBlock) {
 }
 
 TEST_F(NginxConfigParserTest, InvalidLeftParenthesisConfig) {
-  bool success = parser.Parse("invalid_left_parenthesis_config", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/invalid_left_parenthesis_config", &out_config);
   EXPECT_FALSE(success);
 }
 
 TEST_F(NginxConfigParserTest, GetPortNumber) {
-  bool success = parser.Parse("test_port_number", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/test_port_number", &out_config);
 
   int port = out_config.get_port_from_config(&out_config);
   bool same = port==80;
@@ -132,7 +132,7 @@ TEST_F(NginxConfigParserTest, GetPortNumber) {
 }
 
 TEST_F(NginxConfigParserTest, GetPortNumberNull) {
-  bool success = parser.Parse("empty_config", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/empty_config", &out_config);
 
   int port = out_config.get_port_from_config(&out_config);
   bool same = port==-1;
@@ -141,7 +141,7 @@ TEST_F(NginxConfigParserTest, GetPortNumberNull) {
 }
 
 TEST_F(NginxConfigParserTest, NestedPort) {
-  bool success = parser.Parse("nested_port", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/nested_port", &out_config);
 
   int port = out_config.get_port_from_config(&out_config);
   bool same = port==8080;
@@ -150,7 +150,7 @@ TEST_F(NginxConfigParserTest, NestedPort) {
 }
 
 TEST_F(NginxConfigParserTest, InvalidPort) {
-  bool success = parser.Parse("invalid_port", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/invalid_port", &out_config);
 
   int port = out_config.get_port_from_config(&out_config);
   bool same = port==-1;
@@ -159,7 +159,7 @@ TEST_F(NginxConfigParserTest, InvalidPort) {
 }
 
 TEST_F(NginxConfigParserTest, LargePortNumber) {
-  bool success = parser.Parse("large_port_config", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/large_port_config", &out_config);
 
   int port = out_config.get_port_from_config(&out_config);
   bool same = port==-1;
@@ -168,7 +168,7 @@ TEST_F(NginxConfigParserTest, LargePortNumber) {
 }
 
 TEST_F(NginxConfigParserTest, NegativePortNumber) {
-  bool success = parser.Parse("negative_port_config", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/negative_port_config", &out_config);
 
   int port = out_config.get_port_from_config(&out_config);
   bool same = port==-1;
@@ -177,7 +177,7 @@ TEST_F(NginxConfigParserTest, NegativePortNumber) {
 }
 
 TEST_F(NginxConfigParserTest, GetPortNumberNoFile) {
-  bool success = parser.Parse("unexist_config", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/unexist_config", &out_config);
 
   int port = out_config.get_port_from_config(&out_config);
   bool same = port==-1;
@@ -186,7 +186,7 @@ TEST_F(NginxConfigParserTest, GetPortNumberNoFile) {
 }
 
 TEST_F(NginxConfigParserTest, GetPortNumberNullptr) {
-  bool success = parser.Parse("example_config", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/example_config", &out_config);
 
   int port = out_config.get_port_from_config(NULL);
   bool same = port==-1;
@@ -195,7 +195,7 @@ TEST_F(NginxConfigParserTest, GetPortNumberNullptr) {
 }
 
 TEST_F(NginxConfigParserTest, GetPortNumberNoPort) {
-  bool success = parser.Parse("no_port_config", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/no_port_config", &out_config);
 
   int port = out_config.get_port_from_config(&out_config);
   bool same = port==-1;
@@ -204,46 +204,46 @@ TEST_F(NginxConfigParserTest, GetPortNumberNoPort) {
 }
 
 TEST_F(NginxConfigParserTest, SingleQuote) {
-  bool success = parser.Parse("single_quote", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/single_quote", &out_config);
   EXPECT_TRUE(success);
 }
 
 TEST_F(NginxConfigParserTest, DoubleQuote) {
-  bool success = parser.Parse("double_quote", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/double_quote", &out_config);
   EXPECT_TRUE(success);
 }
 
 TEST_F(NginxConfigParserTest, SingleQuoteError) {
-  bool success = parser.Parse("single_quote_error", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/single_quote_error", &out_config);
   EXPECT_FALSE(success);
 }
 
 TEST_F(NginxConfigParserTest, DoubleQuoteError) {
-  bool success = parser.Parse("double_quote_error", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/double_quote_error", &out_config);
   EXPECT_FALSE(success);
 }
 
 TEST_F(NginxConfigParserTest, StartBlockError) {
-  bool success = parser.Parse("start_block_error", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/start_block_error", &out_config);
   EXPECT_FALSE(success);
 }
 
 TEST_F(NginxConfigParserTest, StatementEndError) {
-  bool success = parser.Parse("statement_end_error", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/statement_end_error", &out_config);
   EXPECT_FALSE(success);
 }
 
 TEST_F(NginxConfigParserTest, EndBlockError) {
-  bool success = parser.Parse("end_block_error", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/end_block_error", &out_config);
   EXPECT_FALSE(success);
 }
 
 TEST_F(NginxConfigParserTest, EofSingleQuote) {
-  bool success = parser.Parse("endl_single_quote", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/endl_single_quote", &out_config);
   EXPECT_FALSE(success);
 }
 
 TEST_F(NginxConfigParserTest, EofDoubleQuote) {
-  bool success = parser.Parse("endl_double_quote", &out_config);
+  bool success = parser.Parse("ConfigParserTestFiles/endl_double_quote", &out_config);
   EXPECT_FALSE(success);
 }
