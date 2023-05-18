28ddcfb71a8d3340edce7ad43f420fad0e33c167
Devel User <sahildani@g.ucla.edu>
2023-05-18 05:13:47 +0000

    What: Modified unit testing script for echo_handler. Why: We had originally commented out this when modifying echo handler to meet common API. So, I fixed the unit tests to work with the new echo handler code that matches the class API. Coverage report is low for this one since session test is still in the coverage report but not yet modified to match new session.cc file. Will update that in next commit.
    Change-Id: I7df348c600798ffa3bb2b96061442cfb969f1b05

--- tests/echo_handler_test.cc
+++ tests/echo_handler_test.cc
@@ -1,26 +1,86 @@
+#include <cstring>
+#include <iostream>
+#include <boost/asio/buffers_iterator.hpp>
+
 #include "gtest/gtest.h"
 #include "echo_handler.h"
-#include "gmock/gmock.h"
-#include "config_parser.h"
 
-class echo_handlerFixture : public ::testing::Test {
-  protected:
-    // boost::asio::io_service io_service;
-};
+// Test for empty request body.
+TEST(echoHandlerTest, emptyRequest)
+{
+  // Sample request to test.
+  boost::beast::http::request<boost::beast::http::dynamic_body> request_;  
+  request_.target("/echo");
 
-//TODO FIX TESTS AFTER IMPLEMENTATION
+  std::ostringstream ostring;
+  ostring << request_;
+  std::string request_string = ostring.str();
 
-// TEST_F(echo_handlerFixture, Handler) {
-// 	bool except_found = false;
-// 	boost::asio::io_service io_service;
-// 	tcp::socket socket(io_service);
-// 	try {
-// 		echo_handler handler("");
-// 		handler.parse("GET\n\n");
-// 		handler.handle_request(socket);	
-// 	}
-// 	catch (...) {
-// 		except_found = true;
-// 	}
-//   		EXPECT_TRUE(except_found); 
-// }
+  // Get the return reply struct from the handler function call.
+  boost::beast::http::response<boost::beast::http::dynamic_body> answer;
+  echo_handler handler("/echo", "test url");
+  boost::beast::http::status status_ = handler.serve(request_, answer);
+
+  std::string body { boost::asio::buffers_begin(answer.body().data()),
+                     boost::asio::buffers_end(answer.body().data()) };
+
+  std::vector<std::pair<std::string, std::string>> headers;
+  for(auto const& field : answer)
+  {
+    std::pair<std::string, std::string> header;
+    header.first = std::string(field.name_string());
+    header.second = std::string(field.value());
+    headers.push_back(header);
+  }
+
+  // Check reply struct correctness.
+  bool success = (answer.result() == boost::beast::http::status::ok &&
+                  body == request_string &&
+                  answer.has_content_length() &&
+                  headers.at(1).first == "Content-Type" &&
+                  headers.at(1).second == "text/plain" &&
+                  status_ == answer.result());
+
+  EXPECT_TRUE(success);
+}
+
+// Test for normal request body.
+TEST(echoHandlerTest, normalRequest)
+{
+  // Sample request to test.
+  boost::beast::http::request<boost::beast::http::dynamic_body> request_;  
+  request_.target("/echo");
+  beast::ostream(request_.body()) << "test request";
+
+  std::ostringstream ostring;
+  ostring << request_;
+  std::string request_string = ostring.str();
+
+  // Get the return reply struct from the handler function call.
+  boost::beast::http::response<boost::beast::http::dynamic_body> answer;
+  echo_handler handler("/echo", "test url");
+  boost::beast::http::status status_ = handler.serve(request_, answer);
+
+  std::string body { boost::asio::buffers_begin(answer.body().data()),
+                     boost::asio::buffers_end(answer.body().data()) };
+
+  std::vector<std::pair<std::string, std::string>> headers;
+  for(auto const& field : answer)
+  {
+    std::pair<std::string, std::string> header;
+    header.first = std::string(field.name_string());
+    header.second = std::string(field.value());
+    headers.push_back(header);
+  }
+
+  std::cout << answer << std::endl;
+  // Check reply struct correctness.
+  bool success = (answer.result() == boost::beast::http::status::ok &&
+                  body == request_string &&
+                  answer.has_content_length() && 
+                  headers.at(1).first == "Content-Type" &&
+                  headers.at(1).second == "text/plain" &&
+                  status_ == answer.result());
+
+  EXPECT_TRUE(success);
+}
