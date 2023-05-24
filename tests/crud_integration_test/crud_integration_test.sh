9ef1c6f8afbd50f098ad28198b1290997d6c1ab2
Timothy Poon <timothypoon520@g.ucla.edu>
2023-05-24 07:25:16 +0000

    finish integration test
    Added testing for the delete operation, as well as testing persistence between server restarts.
    
    Change-Id: Ied59a40b08737483911615ca497c64b6fa44fe48

--- tests/crud_integration_test/crud_integration_test.sh
+++ tests/crud_integration_test/crud_integration_test.sh
@@ -9,9 +9,6 @@ SERVER_PID=$!
 SERVER_IP=localhost
 SERVER_PORT=900
 
-#timeout variable
-TIMEOUT=0.2
-
 #Make sure server is fully running
 sleep 1
 
@@ -23,11 +20,11 @@ curl -sSo temp_file_test1 "${SERVER_IP}:${SERVER_PORT}/resource/Tests/1"
 
 diff temp_file_test1 crud_integration_test/expected_1
 
-TEST_1_RESULT=0
-
 RESULT=$?
 
-if [ $RESULT -eq 0 ];
+TEST_1_RESULT=0
+
+if [ $? -eq 0 ];
 then
    TEST_1_RESULT=1
    echo "Test 1 Success"
@@ -35,13 +32,56 @@ else
    echo "Test 1 Fail"
 fi
 
+#Test 2: Restart server and make sure retrieve still works
+kill $SERVER_PID
+../build/bin/webserver ../config/crud_integration_test.conf &
+
+SERVER_PID=$!
+sleep 1
+
+curl -sSo temp_file_test2 "${SERVER_IP}:${SERVER_PORT}/resource/Tests/1"
+diff temp_file_test2 crud_integration_test/expected_1
+
+RESULT=$?
+
+TEST_2_RESULT=0
+
+if [ $RESULT -eq 0 ];
+then
+   TEST_2_RESULT=1
+   echo "Test 2 Success"
+else
+   echo "Test 2 Fail"
+fi
+
+#Test 3: Delete resource and make sure retrieve stops working
+curl -sS -X DELETE "${SERVER_IP}:${SERVER_PORT}/resource/Tests/1"
+curl -sSo temp_file_test3 "${SERVER_IP}:${SERVER_PORT}/resource/Tests/1"
+
+diff temp_file_test3 crud_integration_test/expected_2
+
+RESULT=$?
+
+TEST_3_RESULT=0
+
+if [ $RESULT -eq 0 ];
+then
+   TEST_3_RESULT=1
+   echo "Test 3 Success"
+else
+   echo "Test 3 Fail"
+fi
+
+
 #Clean up (remove temp files and kill server)
 kill $SERVER_PID
 rm -rf resources
 rm temp_file_test1
+rm temp_file_test2
+rm temp_file_test3
 
 #Either exit 0 or exit 1 if tests pass or not
-if [ $TEST_1_RESULT -eq 1 ]; then
+if [ $TEST_1_RESULT -eq 1 ] && [ $TEST_2_RESULT -eq 1 ] && [ $TEST_3_RESULT -eq 1 ]; then
     echo "All tests passed!"
     exit 0
 else
